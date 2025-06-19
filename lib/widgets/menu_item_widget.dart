6b39eaa3f2d646777cfe2f15c3db7b60ea82eb02
AbdAlftahSalem <abd321alftah@gmail.com>
2025-06-19 10:23:58 +0300

    Update padding style

--- lib/widgets/menu_item_widget.dart
+++ lib/widgets/menu_item_widget.dart
@@ -21,7 +21,7 @@ class MenuItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+      padding: const EdgeInsets.all(8.0),
       child: InkWell(
         onTap: () => onTap(item, index),
         child: Row(
